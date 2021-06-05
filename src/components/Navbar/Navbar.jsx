56d4fce8cfc3463921c9d97b939a72f14994a106
Victor2043 <36427527+Victor2043@users.noreply.github.com>
2021-06-05 12:33:00 -0300

    add icon in tab

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = ({ totalItems }) => {
             <Toolbar>
                 <Typography component={Link} to="/carrinho" variant="h6" className={classes.title} color="inherit">
                     <img src={logo} alt="Commerce.js" height="25px" className={classes.image}/>
-                   Loja de eletronicos
+                   Loja de eletrônicos
                 </Typography>
                 <div className={classes.grow}/>
                 {location.pathname === "/" && (
